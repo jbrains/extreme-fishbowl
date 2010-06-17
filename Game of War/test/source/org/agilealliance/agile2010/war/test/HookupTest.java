a29440d814ddf426d978d2f5a2719f039defb2d3
J. B. Rainsberger <me@jbrains.ca>
2010-06-17 13:44:53 -0300

    Empty project with failing hookup test.

---
+++ Game of War/test/source/org/agilealliance/agile2010/war/test/HookupTest.java
@@ -0,0 +1,11 @@
+package org.agilealliance.agile2010.war.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HookupTest {
+	@Test
+	public void hookup() throws Exception {
+		Assert.fail("I got here");
+	}
+}
